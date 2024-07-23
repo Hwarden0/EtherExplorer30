413faf9b3892fd51b5ed9b752ea3678bd589a9e8
hishammohamed101 <hm430009@gmail.com>
2024-07-23 15:27:04 +0300

    just add the ipfs & uri for my dog

---
+++ script/Interaction.s.sol
@@ -0,0 +1,24 @@
+    //SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/Layer1/BasicNft.sol";
+import {console} from "forge-std/console.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address contractAddress = vm.envAddress("BASIC_NFT_ADDRESS");
+        require(contractAddress != address(0), "No contract address provided");
+        mintNftOnContract(contractAddress);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
